6d950be45def251dd9cf9ad772fdb579a94e13db
Kevin Lee <kevin.lee@tubemogul.com>
2015-08-06 14:24:05 -0700

    devops error

--- src/main/java/com/tubemogul/platform/tubebnb/TubeBnbServer.java
+++ src/main/java/com/tubemogul/platform/tubebnb/TubeBnbServer.java
@@ -1,11 +1,17 @@
 package com.tubemogul.platform.tubebnb;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.github.nhuray.dropwizard.spring.SpringBundle;
+import com.tubemogul.devops.dropwizard.DevopsBundle;
+import com.tubemogul.devops.spring.SpringSystemContext;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
@@ -15,7 +21,8 @@ import java.util.Arrays;
 public class TubeBnbServer extends Application<TubeBnbServerConfiguration> {
 
     private static final Logger logger = LoggerFactory.getLogger(TubeBnbServer.class);
-
+    private final SpringSystemContext systemContext = new SpringSystemContext();
+    private final ConfigurableApplicationContext applicationContext = applicationContext();
 
 
     @Override
@@ -31,6 +38,8 @@ public class TubeBnbServer extends Application<TubeBnbServerConfiguration> {
 
     @Override
     public void initialize(Bootstrap<TubeBnbServerConfiguration> bootstrap) {
+        bootstrap.addBundle(new DevopsBundle(getSystemContext()));
+        bootstrap.addBundle(new SpringBundle<TubeBnbServerConfiguration>(applicationContext, true, true, true));
 
     }
 
@@ -40,5 +49,20 @@ public class TubeBnbServer extends Application<TubeBnbServerConfiguration> {
 
 //        environment.jersey().register(new TmParameterExceptionMapper());
         environment.getObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+    }
+
+    private ConfigurableApplicationContext applicationContext() throws BeansException {
+        SpringSystemContext sctx = getSystemContext();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        context.getBeanFactory().registerSingleton("systemContext", sctx);
+        context.scan(getClass().getPackage().getName());
+        context.registerShutdownHook();
+
+        return context;
+    }
+
+    public SpringSystemContext getSystemContext() {
+        return systemContext;
     }
 }
